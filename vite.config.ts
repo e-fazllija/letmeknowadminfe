0c09abcd23170b661503c5e856eb0af548269b68
Lorenzo Cani <l.cani@gruppointent.com>
2025-11-04 14:45:36 +0100

    chore: update dev server port to 5175

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174,
+    port: 5175,
     proxy: {
       '/v1': {
         target: 'http://localhost:3000',
